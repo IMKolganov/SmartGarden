ed7b2677058e59a6c468e47e192c45621163727d
Ivan Kolganov <imkolganov@gmail.com>
2024-09-13 04:52:12 +0300

    fix problem with docker on Windows.
    sed -i 's/\r//' /usr/local/bin/rabbitmq-plugins.sh

--- rabbitmq/rabbitmq-plugins.sh
+++ rabbitmq/rabbitmq-plugins.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
+set -x
 
+echo "rabbit-pugins.sh Starting plugin configuration..."
+
+# Enable plugins
 rabbitmq-plugins enable rabbitmq_mqtt
 rabbitmq-plugins enable rabbitmq_web_mqtt
+
+echo "rabbit-pugins.sh Plugins enabled successfully."
